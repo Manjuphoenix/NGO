ef766ec7082d3625b66b291c3bce5c0ad78bbb8b
Manjunath D <56871357+Manjuphoenix@users.noreply.github.com>
2020-11-28 23:29:46 +0530

    Add files via upload

--- ngo/app1/models.py
+++ ngo/app1/models.py
@@ -7,3 +7,34 @@ class cause(models.Model):
     raised = models.IntegerField
     img = models.ImageField(upload_to = 'pics')
     ext_link = models.CharField(max_length=100)
+
+class Volunteer(models.Model):
+    name = models.CharField(max_length=20)
+    country = models.CharField(max_length=15)
+    phone = models.IntegerField
+    email = models.EmailField
+
+class Contributors(models.Model):
+    dob = models.IntegerField
+    name = models.CharField(max_length=20)
+    country = models.CharField(max_length=15)
+    phone = models.IntegerField
+    email = models.EmailField
+
+class Objects(models.Model):
+    obj_id = models.IntegerField
+    obj_name = models.CharField(max_length=15)
+    obj_price = models.IntegerField
+
+
+class Contributions(models.Model):
+    name = models.CharField(max_length=15)
+    age = models.IntegerField
+    amt = models.IntegerField
+    nationality = models.CharField(max_length=20)
+
+class Events(models.Model):
+    event_id = models.IntegerField
+    title = models.CharField
+    date = models.DateField
+    place = models.CharField(max_length=20)
