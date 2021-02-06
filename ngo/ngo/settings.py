630ca2505b297eef758921e1d94c8d0efa5c19eb
Manjunath D <56871357+Manjuphoenix@users.noreply.github.com>
2021-02-06 21:26:20 +0530

    settings.py for deploying on heroku

--- ngo/ngo/settings.py
+++ ngo/ngo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7dekvz2jr2$yer-9&hhlj=rhuen!x@6hm^1(wfady8a3utj35c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
